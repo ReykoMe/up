6e2a5fe1be4ed70da09c9a3b54c908b46dbffaea
ReykoMe <andrbrd@gmail.com>
2023-11-21 19:52:47 +0200

    [pkg[: add libraries for testing, configure testing environment

--- react-app/src/declarations/vite-env.d.ts
+++ react-app/src/declarations/vite-env.d.ts
@@ -1,5 +1,6 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
+/// <reference types="vitest" />
 
 interface ImportMetaEnv {
   readonly VITE_API_KEY: string;
